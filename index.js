199692f38c00ab0cf7e2a13d4a67f6f12a2734a6
Caio Ronei <crsc2002@gmail.com>
2026-02-17 20:19:02 -0300

    feats:| html- added an id for task section 'tarefas'.| javascript- added buscarTarefa function, which searches tasks in local api list using fetch and .then arrow functions, added criarTarefa function, in order to insert a new li element whenever a new task is registered in the api array.| Refactor: refactored a few functions, closing them and preventing future errors.

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 function novaTarefa(){
-    overlay.classList.add("active")
-    criarTarefa.classList.add("active")
+    overlay.classList.add("active");
+    criarTarefa.classList.add("active");
 }
 
 function fecharModal(){
-    overlay.classList.remove("active")
-    criarTarefa.classList.remove("active")
+    overlay.classList.remove("active");
+    criarTarefa.classList.remove("active");
+}
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(res => {
+        criarTarefa(res);
+    })
+} buscarTarefas();
+
+function criarTarefa(listaDeTarefas){
+    if(listaDeTarefas.length > 0){
+        listaDeTarefas.map(tarefa => {
+            lista.innerHTML = ""
+            lista.innerHTML += `
+                <li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <div class="actions">
+                        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  
+                        fill="#999" viewBox="0 0 24 24" ><path d="M17 6V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v2H2v2h2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8h2V6zM9 4h6v2H9zM6 20V8h12v12z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
+                        </svg>
+                    </div>
+                </li>
+                `;
+        })
+    }
 }
